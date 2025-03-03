de5c4dd80221007607fd5fe96c7d0783d541bd81
Jch4ipas <jeremy.wuersch@gmail.com>
2025-03-03 14:03:14 +0100

    [feature] add modular box

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
 import Button from "@/components/button";
 import UserStatisticsCard from "@/components/graphic";
 import PieChartComponent from "@/components/piechart";
@@ -6,21 +8,28 @@ import RadialChartComponent from "@/components/radialchart";
 import NasaMedia from "@/services/infonasa"
 
 export default function Home() {
+  const boxes = [
+    { id: 1, width: 2, height: 4, content: <p className="flex text-xl font-bold items-center justify-center">Hello World</p>  },
+    { id: 2, width: 2, height: 2, content: <iframe src="https://grafana-prod-route-grafana-prod.apps.ocpitsp0001.xaas.epfl.ch/d/wpngrfndshbrda/e29cb0-wpn-e29cb0?orgId=1&from=now-12h&to=now&timezone=browser&refresh=30s" className="w-full h-full"></iframe>},
+    { id: 3, width: 2, height: 2, content: <RadialChartComponent></RadialChartComponent>},
+    { id: 4, width: 4, height: 2, content: <NasaMedia></NasaMedia> },
+  ];
+
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="grid grid-cols-3 gap-8 p-8 w-full h-full min-h-screen">
-        <div className="justify-center items-center gap-4">
-          <h1 className="flex text-xl font-bold items-center justify-center">Hello World !</h1>
-        </div>
-        
-        <div className="border border-gray-600 rounded-lg flex justify-center items-center"><NasaMedia></NasaMedia></div>
-        <div className="border border-gray-600 rounded-lg flex justify-center items-center"><iframe src="https://jch4ipas.github.io/kata-APOD" className="w-full h-full"></iframe></div>
-        <div className="border border-gray-600 rounded-lg flex justify-center items-center"><RadialChartComponent></RadialChartComponent></div>
-        <div className="border border-gray-600 rounded-lg flex justify-center items-center"><iframe 
-          src="https://grafana-prod-route-grafana-prod.apps.ocpitsp0001.xaas.epfl.ch/dashboards" 
-          className="w-full h-full"
-        ></iframe></div>
-        <div className="border border-gray-600 rounded-lg flex justify-center items-center"><h1>ooh Hello</h1></div>
+    <div className="h-screen w-full">
+      <div className="grid grid-cols-6 grid-rows-4 gap-2 w-full h-full p-2">
+        {boxes.map((box) => (
+          <div
+            key={box.id}
+            className="border border-gray-600 rounded-3xl flex justify-center items-center text-white font-bold shadow-md p-2"
+            style={{
+              gridColumn: `span ${box.width}`,
+              gridRow: `span ${box.height}`,
+            }}
+          >
+            {box.content}
+          </div>
+        ))}
       </div>
     </div>
   );
